88ec87ad9d58ed32f93101c0763fefd945e22996
lnssr <gifzoner5@gmail.com>
2024-06-16 19:24:15 +0300

    nf

--- src/test/test.sh
+++ src/test/test.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bats
 
 setup() {
-    # Создание временной директории для тестов
     TEST_DIR=$(mktemp -d)
     HOME_DIR=$(mktemp -d)
 }
 
 teardown() {
-    # Удаление временной директории после тестов
     rm -rf "$TEST_DIR"
     rm -rf "$HOME_DIR"
 }
@@ -17,12 +15,6 @@ teardown() {
     [ "$status" -eq 1 ]
     [[ "$output" == *"Не указан путь к папке для бекапов."* ]]
 }
-
-#@test "Скрипт устанавливает зависимости" {
-    # Мокаем команду установки зависимостей
-#    run bash backup.sh "$TEST_DIR"
-#    [ "$status" -eq 0 ]
-#}
 
 @test "Скрипт создает архив" {
     HOME="$HOME_DIR" run bash backup.sh "$TEST_DIR"
